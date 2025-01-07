f4e226d22aa33c9f57cd6042ba0c6a51b27453d2
Jaewon <wellbird@naver.com>
2025-01-07 21:04:53 +0900

    FEAT : util todo 구현
    - 금액이 현재 자산보다 이하인지 체크
    - 값이 존제하는지 체크

--- js/util.js
+++ js/util.js
@@ -1,17 +1,15 @@
 export function toShow(node) {
-  node.className = node.className.replace("v-none", "v-show");
+  node.className = node.className.replace('v-none', 'v-show');
 }
 
 export function toHidden(node) {
-  node.className = node.className.replace("v-show", "v-none");
+  node.className = node.className.replace('v-show', 'v-none');
 }
 
 export function validatePrice(currentFunds, currentAmount) {
-  // TODO: 금액이 현재 자산보다 이하인지
-  return true;
+  return currentFunds >= currentAmount;
 }
 
 export function validateRequired({ category, description, price }) {
-  // TODO: 값이 존재하는지
-  return true;
+  return Boolean(category) && Boolean(description) && Boolean(price) && price > 0;
 }
