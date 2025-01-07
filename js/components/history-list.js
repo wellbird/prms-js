a9d326785c152b45c317a710ee7822a4faf2261d
Jaewon <wellbird@naver.com>
2025-01-07 21:05:43 +0900

    FEAT : history-list todo 구현
    - 데이터 매핑
    - 오름차순 정렬
    - 시간 포맷 변경
    - 금액 콤마 포맷 맞추기

--- js/components/history-list.js
+++ js/components/history-list.js
@@ -1,7 +1,7 @@
-import { renderCurrentAsset } from "../components/current-asset";
-import { store, removeHistory } from "../store";
+import { renderCurrentAsset } from '../components/current-asset';
+import { store, removeHistory } from '../store';
 
-const $sectionHistory = document.querySelector(".history");
+const $sectionHistory = document.querySelector('.history');
 
 export function initHistoryList() {
   renderHistoryList();
@@ -9,15 +9,15 @@ export function initHistoryList() {
 }
 
 function addHistoryListEventListener() {
-  $sectionHistory.addEventListener("click", function (event) {
+  $sectionHistory.addEventListener('click', function (event) {
     const element = event.target;
-    if (!element.className.includes("delete-button")) return;
+    if (!element.className.includes('delete-button')) return;
 
     const { dateid, itemid } = element.dataset;
 
     const isSuccess = removeHistory(dateid, itemid);
     if (!isSuccess) {
-      alert("소비내역 삭제에 실패했습니다.");
+      alert('소비내역 삭제에 실패했습니다.');
       return;
     }
 
@@ -31,46 +31,47 @@ function reRender() {
 }
 
 export function renderHistoryList() {
-  // TODO: 데이터 매핑
-  // TODO: 오름차순으로 목록 나열
-  // TODO: 항목의 시간 포맷 변경: `HH:mm`
-  // TODO: 금액 콤마 포맷 맞추기
-
   $sectionHistory.innerHTML = store.dateList
     .map(({ date, id: dateId }) => {
       const detail = store.detailList[dateId];
-      if (!detail?.length) return "";
+      if (!detail?.length) return '';
 
       return `<article class="history-per-day">
       <p class="history-date">2021년 12월 1일</p>
-      <section class="history-item">
+      ${detail
+        .sort((a, b) => b.id - a.id)
+        .map(({ description, category, amount, fundsAtTheTime, createAt, id }) => {
+          const time = new Date(createAt).toLocaleTimeString('ko-kr', { timeStyle: 'short', hourCycle: 'h24' });
+          return `<section class="history-item">
         <section class="history-item-column">
-          <div class="create-at">10:30</div>
+          <div class="create-at">${time}</div>
           <div class="history-detail">
             <div class="history-detail-row history-detail-title">
-              <p>아이스 아메리카노</p>
+              <p>${description}</p>
             </div>
             <div class="history-detail-row history-detail-subtitle">
-              <p>카페</p>
+              <p>${category}</p>
               <p>
-                1000000
+                ${amount.toLocaleString()}
                 <span>원</span>
               </p>
             </div>
           </div>
           <div class="delete-section">
-            <button class="delete-button">🗑</button>
+            <button class="delete-button" data-dateid=${dateId} data-itemid=${id}>🗑</button>
           </div>
         </section>
         <section class="history-item-caption">
           <p>
             <span>남은 자산</span>
-            <span>300000</span>
+            <span>${fundsAtTheTime.toLocaleString()}</span>
             <span>원</span>
           </p>
         </section>
-      </section>
+      </section>`;
+        })
+        .join('')}
     </article>`;
     })
-    .join("");
+    .join('');
 }
